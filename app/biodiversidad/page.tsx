753fc62cf1f5d8104a474269b9950dacd71bbc74
Katherine Stehberg <kstehberg@gmail.com>
2025-01-02 23:09:54 -0300

    update

--- app/biodiversidad/page.tsx
+++ app/biodiversidad/page.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Link from 'next/link'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card"
 import { ChevronRight, Leaf, Fish, Bird, Bug } from 'lucide-react'
